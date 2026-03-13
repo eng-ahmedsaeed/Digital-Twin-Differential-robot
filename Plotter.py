f60a0e2f50f1e48a81f67c67a8484569892f616c
eng-ahmedsaeed <ahmedsaeed.ah101@gmail.com>
2026-03-14 01:42:17 +0200

    visualizer first edition

--- Plotter.py
+++ Plotter.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pygame 
 class Plot:
     def __init__(self,dt):
         plt.ion()
@@ -115,9 +117,185 @@ class Plot:
     )
 
 
-class Visualization :
-    def __init__(self):
-        pass
 
 
+class Visualization:
 
+    def __init__(self, dt):
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((700, 600))
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+        self.dt = dt
+        self.accum = 0
+
+        self.max_points = 100
+
+        self.robot_points = [None]*self.max_points
+        self.Path_points = [None]*self.max_points
+
+        self.front = 0
+        self.rear = 0
+
+        self.theta = 0
+
+        self.robot = pygame.image.load("assets/car.PNG")
+
+        w = self.robot.get_width()
+        h = self.robot.get_height()
+
+        self.robot = pygame.transform.scale(self.robot,(w//50 , h//50))
+        self.rect = self.robot.get_rect()
+
+        # عرض الخط = عرض العربية
+        self.line_width = self.robot.get_width()
+
+        # surface للـ shadow
+        self.shadow_surface = pygame.Surface(
+            self.screen.get_size(),
+            pygame.SRCALPHA
+        )
+
+
+    def Get_points(self):
+
+        return np.random.rand()*10,4,45
+
+
+    def Play(self):
+
+        plot_right = self.screen.get_width()*3/4
+        center_y = self.screen.get_height()/2
+
+        while self.running:
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+
+            self.screen.fill("black")
+            self.shadow_surface.fill((0,0,0,0))
+
+
+            # physics
+            while self.accum >= self.dt:
+
+                robot_y, path_y, self.theta = self.Get_points()
+
+                self.robot_points[self.rear] = (0,robot_y)
+                self.Path_points[self.rear] = (0,path_y)
+
+                self.rect.center = (
+                    plot_right,
+                    center_y + robot_y
+                )
+
+                if self.rear >= self.max_points-1:
+
+                    self.front = (self.front + 1) % self.max_points
+                    self.rear = 0
+
+                else:
+
+                    self.rear += 1
+
+
+                self.accum -= self.dt
+
+
+            rotated = pygame.transform.rotate(self.robot,self.theta)
+            rect = rotated.get_rect(center=self.rect.center)
+
+            robot_draw = []
+            path_draw = []
+
+            count = (self.rear-self.front) % self.max_points
+
+            if count == 0:
+                count = 1
+
+            step = plot_right / count
+
+            t = 0
+            i = (self.rear-1) % self.max_points
+
+
+            while True:
+
+                if self.robot_points[i] is not None:
+
+                    _, ry = self.robot_points[i]
+                    _, py = self.Path_points[i]
+
+                    robot_draw.append(
+                        (plot_right - t, center_y + ry)
+                    )
+
+                    path_draw.append(
+                        (plot_right - t, center_y + py)
+                    )
+
+                    t += step
+
+
+                if i == self.front:
+                    break
+
+                i = (i - 1) % self.max_points
+
+
+            # رسم path shadow
+            if len(path_draw) > 1:
+
+                pygame.draw.lines(
+                    self.shadow_surface,
+                    (0,255,0,80),  # alpha = shadow
+                    False,
+                    path_draw,
+                    self.line_width
+                )
+
+
+            # رسم robot line
+            if len(robot_draw) > 1:
+
+                pygame.draw.lines(
+                    self.screen,
+                    "white",
+                    False,
+                    robot_draw,
+                    self.line_width
+                )
+
+
+            # عرض shadow layer
+            self.screen.blit(self.shadow_surface,(0,0))
+
+
+            # رسم العربية
+            self.screen.blit(rotated,rect)
+
+
+            pygame.display.update()
+
+            frame_dt = self.clock.tick(60)/1000
+            self.accum += frame_dt
+
+
+        pygame.quit()
+
+
+########################## test ##########################
+
+def main():
+
+    v = Visualization(0.1)
+    v.Play()
+
+
+main()
